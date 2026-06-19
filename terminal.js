12256c03913d2d8ad8e7b9c3afb78a14b8faba71
samarth chugh <samarth.chugh@avmschools.ac.in>
2026-06-19 19:41:29 +0530

    Create terminal.js

---
+++ terminal.js
@@ -0,0 +1,24 @@
+help
+
+ls
+pwd
+
+cd Projects
+cd Robotics
+
+cat robot-notes.txt
+
+mkdir Games
+touch test.txt
+rm test.txt
+
+open notes
+open calculator
+
+whoami
+neofetch
+
+date
+time
+
+clear
